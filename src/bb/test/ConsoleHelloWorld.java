dabbf2218a91939e46781a455e0d3d79450728b1
Ashton Fedler <ashtonfedler@gmail.com>
2011-03-05 13:18:05 -0800

    Created a hello world console application to verify the eclipse project is working on the various host systems.

---
+++ src/bb/test/ConsoleHelloWorld.java
@@ -0,0 +1,20 @@
+package bb.test;
+
+/**
+ * This file is just to test that your eclipse architecture has
+ * been initialized correctly by running a simple "Hello World"
+ * console application.
+ * 
+ * @author  Ashton Fedler
+ * @email   fedler@unbc.ca
+ * @date    March 5th 2011
+ * @license GPLv3
+ */
+
+public class ConsoleHelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.printf("Hello World");
+	}
+}
